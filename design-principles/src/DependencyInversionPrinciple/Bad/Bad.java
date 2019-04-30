4bb39cf047162f73ed782bb35528d7ce1f6a7b01
Duban Cano <ducano@bancolombia.com.co>
2019-04-29 23:31:46 -0500

    GRAL: Se agrega ejemplo de la inversion de despendencia

--- design-principles/src/DependencyInversionPrinciple/Bad/Bad.java
+++ design-principles/src/DependencyInversionPrinciple/Bad/Bad.java
@@ -3,3 +3,38 @@ package DependencyInversionPrinciple.Bad;
 public class Bad {
 
 }
+
+// Dependency Inversion Principle - Bad example
+
+class Worker {
+
+    public void work() {
+
+        // ....working
+
+    }
+
+}
+
+
+
+class Manager {
+
+    Worker worker;
+
+
+
+    public void setWorker(Worker w) {
+        worker = w;
+    }
+
+    public void manage() {
+        worker.work();
+    }
+}
+
+class SuperWorker {
+    public void work() {
+        //.... working much more
+    }
+}
--- design-principles/src/DependencyInversionPrinciple/Good/Good.java
+++ design-principles/src/DependencyInversionPrinciple/Good/Good.java
@@ -2,3 +2,33 @@ package DependencyInversionPrinciple.Good;
 
 public class Good {
 }
+
+
+// Dependency Inversion Principle - Good example
+interface IWorker {
+    public void work();
+}
+
+class Worker implements IWorker{
+    public void work() {
+        // ....working
+    }
+}
+
+class SuperWorker  implements IWorker{
+    public void work() {
+        //.... working much more
+    }
+}
+
+class Manager {
+    IWorker worker;
+
+    public void setWorker(IWorker w) {
+        worker = w;
+    }
+
+    public void manage() {
+        worker.work();
+    }
+}
